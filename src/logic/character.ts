5d0414616748ca12eb0bd5124b06ef883d188b1f
c0d3hdz <marquezhernandez.damian@gmail.com>
2024-04-19 23:05:29 -0600

    'actualizaciondeimagenesdeYAHIR'

--- src/logic/character.ts
+++ src/logic/character.ts
@@ -8,12 +8,12 @@ export const Characters: Character[] = [
         imageSmall: '/img/Yahir/Yahir-small.webp',
         imagesGalery: [
             {
-                image: '/img/Yahir/Yahir-image-1.webp',
+                image: '/img/Yahir/Yahir-image-5.webp',
                 isVertical: false,
                 class: 'sm:col-start-1 sm:col-end-2 sm:row-start-1 sm:row-end-2',
             },
             {
-                image: '/img/Yahir/Yahir-image-2.webp',
+                image: '/img/Yahir/Yahir-image-6.webp',
                 isVertical: true,
                 class: 'sm:col-start-2 sm:col-end-3 sm:row-start-1 sm:row-end-3 col-start-1 col-end-2 row-start-1 row-end-3',
             },
@@ -23,12 +23,12 @@ export const Characters: Character[] = [
                 class: 'sm:col-start-3 sm:col-end-4 sm:row-start-1 sm:row-end-2',
             },
             {
-                image: '/img/Yahir/Yahir-image-1.webp',
+                image: '/img/Yahir/Yahir-image-4.webp',
                 isVertical: true,
                 class: 'sm:col-start-1 sm:col-end-2 sm:row-start-2 sm:row-end-3',
             },
             {
-                image: '/img/Yahir/Yahir-image-1.webp',
+                image: '/img/Yahir/Yahir-image-7.webp',
                 isVertical: false,
                 class: 'sm:col-start-3 sm:col-end-4 sm:row-start-2 sm:row-end-3',
             },
