d959166272329a59cb095175970802b7f2dc206f
MartL7 <latiapelitos123@gmail.com>
2024-04-20 21:23:39 -0600

    🤍 Exctracted logic of Scripts with const folder

---
+++ src/const/showModal.ts
@@ -0,0 +1,26 @@
+import { $, $$ } from '@/lib/dom-selector'
+
+export const showModal = () => {
+  const $imageContainer = $$('.image-container') as NodeListOf<HTMLImageElement>
+  const $imageClip = $('#image-clip') as HTMLImageElement
+  const $imageDialog = $('.clip-dialog') as HTMLDialogElement
+  const $closeButton = $('#close-dialog') as HTMLButtonElement
+
+  $imageDialog.addEventListener('close', () => {
+    $imageClip.src = ''
+  })
+
+  $imageContainer.forEach((image) => {
+    const { url } = image.dataset
+
+    image.addEventListener('click', () => {
+      const name = url.split('/').at(-2)
+
+      $imageClip.alt = `Imagen de la galeria de ${name}`
+      $imageClip.src = url
+      $imageDialog.showModal()
+
+      $closeButton.onclick = () => $imageDialog.close()
+    })
+  })
+}
