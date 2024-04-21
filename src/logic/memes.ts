9de15f73b4e717446e22cbc5c4763633116ff264
MartL7 <latiapelitos123@gmail.com>
2024-04-20 19:51:44 -0600

    🤍 Change design for Galery of Memes

--- src/logic/memes.ts
+++ src/logic/memes.ts
@@ -4,40 +4,41 @@ export const memesImages: Meme[] = [
   {
     id: 'Meme-1',
     image: '/img/Memes/image-meme-1.avif',
-    class: 'col-start-1 col-end-2 row-start-1 row-end-2'
+    class: 'col-start-1 col-end-2 row-start-1 row-end-3 h-[300px] sm:h-[300px]'
   },
   {
     id: 'Meme-2',
     image: '/img/Memes/image-meme-2.webp',
-    class: 'h-[200px] col-start-1 col-end-2 row-start-2 row-end-3'
+    class: 'sm:h-[200px] sm:col-start-1 sm:col-end-2 sm:row-start-3 sm:row-end-4 col-start-2 col-end-3 row-start-5 row-end-6 h-[200px]'
   },
   {
     id: 'Meme-3',
-    image: '/img/Memes/image-meme-3.avif'
+    image: '/img/Memes/image-meme-3.avif',
+    class: 'sm:col-start-3 sm:col-end-4 sm:row-start-3 sm:row-end-4 sm:h-[200px] col-start-2 col-end-3 row-start-1 row-end-2 h-[200px]'
   },
   {
     id: 'Meme-4',
     image: '/img/Memes/image-meme-4.webp',
-    class: ''
+    class: 'sm:col-start-2 sm:col-end-3 sm:h-[200px] col-start-1 col-end-2 sm:row-start-1 sm:row-end-2 row-start-7 row-end-8 h-[200px]'
   },
   {
     id: 'Meme-5',
     image: '/img/Memes/image-meme-5.avif',
-    class: 'sm:col-start-3 sm:col-end-5 sm:row-start-1'
+    class: 'sm:col-start-4 sm:col-end-5 sm:row-start-1 sm:row-end-2 sm:h-[200px] col-start-1 col-end-2 row-start-3 row-end-5 h-[200px]'
   },
   {
     id: 'Meme-6',
     image: '/img/Memes/image-meme-6.webp',
-    class: 'h-[200px]'
+    class: 'sm:row-start-1 sm:row-end-3 sm:col-start-3 sm:col-end-4 sm:h-[300px] col-start-1 col-end-2 row-start-5 row-end-7 h-[300px]'
   },
   {
     id: 'Meme-7',
     image: '/img/Memes/image-meme-7.webp',
-    class: 'h-[200px]'
+    class: 'sm:row-start-2 sm:row-end-4 sm:col-start-2 sm:col-end-3 sm:h-[300px] col-start-2 col-end-3 row-start-2 row-end-5 h-[300px]'
   },
   {
     id: 'Meme-8',
     image: '/img/Memes/image-meme-8.webp',
-    class: 'col-start-1 col-end-2 row-start-3 row-end-5'
+    class: 'sm:col-start-4 sm:col-end-5 sm:row-start-2 sm:row-end-4 sm:h-[300px] col-start-2 col-end-3 row-start-6 row-end-8 h-[300px]'
   }
 ]
