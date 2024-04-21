ea737808d1f20fe8d3be94f433cd225839b1fa9e
MartL7 <latiapelitos123@gmail.com>
2024-04-21 01:17:51 -0600

    🤍 Update design

--- src/logic/character.ts
+++ src/logic/character.ts
@@ -208,22 +208,22 @@ export const Characters: Character[] = [
             {
                 image: '/img/Damian/Damian-image-1.webp',
                 isVertical: false,
-                class: 'sm:col-start-1 sm:col-end-2 sm:row-start-1 sm:row-end-3 sm:h-[620px]',
+                class: 'sm:col-start-2 sm:col-end-3 sm:row-start-1 sm:row-end-2 sm:h-[300px] col-start-2 col-end-3 row-start-1 row-end-3 h-[400px]',
             },
             {
                 image: '/img/Damian/Damian-image-2.webp',
                 isVertical: false,
-                class: 'sm:h-[300px]',
+                class: 'sm:h-[300px] sm:col-start-3 sm:col-end-4 sm:row-start-1 sm:row-end-2 col-start-1 col-end-2 row-start-2 row-end-4 h-[400px]',
             },
             {
                 image: '/img/Damian/Damian-image-3.webp',
                 isVertical: false,
-                class: 'sm:h-[300px]',
+                class: 'sm:col-start-1 sm:col-end-2 sm:row-start-1 sm:row-end-3 sm:h-[600px] h-[200px] col-start-1 col-end-2 row-start-1 row-end-2',
             },
             {
                 image: '/img/Damian/Damian-image-4.webp',
                 isVertical: false,
-                class: 'sm:h-[300px]',
+                class: 'sm:h-[300px] sm:col-start-2 sm:col-end-3 sm:row-start-2 row-start-3 row-end-4 col-start-2 col-end-3 h-[200px]',
             },
         ],
         funnyText: ['Crecer como los Bisteces', 'A Putazos', 'BOMBARDEN HUAUCHINANGO'],
