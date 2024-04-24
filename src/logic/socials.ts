7979f184a866c4a9f932c67ad0c164ccf7189923
MartL7 <latiapelitos123@gmail.com>
2024-04-24 01:16:41 -0600

    💖 Solution Error for socials map

---
+++ src/logic/socials.ts
@@ -0,0 +1,9 @@
+import GithubIcon from "@/components/icons/Github.astro"
+import Instagram from "@/components/icons/Instagram.astro"
+import TwitterIcon from '@/components/icons/Twitter.astro'
+
+export const socialIcons = {
+  github: GithubIcon,
+  instagram: Instagram,
+  twitter: TwitterIcon
+}
