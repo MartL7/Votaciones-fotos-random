3a33023a55dec90dc1e524758fe469955f7ab2f8
MartL7 <latiapelitos123@gmail.com>
2024-04-27 20:31:47 -0600

    Change login of Github to Google

--- auth.config.js
+++ auth.config.js
@@ -1,11 +1,11 @@
-import GitHub from '@auth/core/providers/github'
+import Google from '@auth/core/providers/google'
 import { defineConfig } from 'auth-astro'
 
 export default defineConfig({
   providers: [
-    GitHub({
-      clientId: import.meta.env.GITHUB_CLIENT_ID,
-      clientSecret: import.meta.env.GITHUB_CLIENT_SECRET,
+    Google({
+      clientId: import.meta.env.GOOGLE_CLIENT_ID,
+      clientSecret: import.meta.env.GOOGLE_CLIENT_SECRET,
     }),
   ],
   callbacks: {
