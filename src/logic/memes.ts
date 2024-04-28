0094bc26bcb946b66058ca4d2ad106ff671e3046
MartL7 <latiapelitos123@gmail.com>
2024-04-28 17:34:56 -0600

    🤍 Rename images for Memes

--- src/logic/memes.ts
+++ src/logic/memes.ts
@@ -3,42 +3,42 @@ import type { Meme } from '@/types/Memes'
 export const memesImages: Meme[] = [
   {
     id: 'Meme-1-la-seduce',
-    image: '/img/Memes/image-meme-yahir-3.webp',
+    image: '/img/Memes/Meme-1-la-seduce.webp',
     class: 'col-start-1 col-end-2 row-start-1 row-end-3 h-[300px] sm:h-[300px]'
   },
   {
     id: 'Meme-2-xd',
-    image: '/img/Memes/image-meme-martin-2.webp',
+    image: '/img/Memes/Meme-2-xd.webp',
     class: 'sm:h-[200px] sm:col-start-1 sm:col-end-2 sm:row-start-3 sm:row-end-4 col-start-2 col-end-3 row-start-5 row-end-6 h-[200px]'
   },
   {
     id: 'Meme-3-sexooo',
-    image: '/img/Memes/image-meme-morro-4.webp',
+    image: '/img/Memes/Meme-3-sexooo.webp',
     class: 'sm:col-start-3 sm:col-end-4 sm:row-start-3 sm:row-end-4 sm:h-[200px] col-start-2 col-end-3 row-start-1 row-end-2 h-[200px]'
   },
   {
     id: 'Meme-4-inspeccion-tetas',
-    image: '/img/Memes/image-meme-yahir-1.webp',
+    image: '/img/Memes/Meme-4-inspeccion-tetas.webp',
     class: 'sm:col-start-2 sm:col-end-3 sm:h-[200px] col-start-1 col-end-2 sm:row-start-1 sm:row-end-2 row-start-7 row-end-8 h-[200px]'
   },
   {
     id: 'Meme-5-cobrese',
-    image: '/img/Memes/image-meme-oaxaco-2.avif',
+    image: '/img/Memes/Meme-5-cobrese.webp',
     class: 'sm:col-start-4 sm:col-end-5 sm:row-start-1 sm:row-end-2 sm:h-[200px] col-start-1 col-end-2 row-start-3 row-end-5 h-[200px]'
   },
   {
     id: 'Meme-6-pou',
-    image: '/img/Memes/image-meme-morro-8.webp',
+    image: '/img/Memes/Meme-6-pou.webp',
     class: 'sm:row-start-1 sm:row-end-3 sm:col-start-3 sm:col-end-4 sm:h-[300px] col-start-1 col-end-2 row-start-5 row-end-7 h-[300px]'
   },
   {
     id: 'Meme-7-bechito',
-    image: '/img/Memes/image-meme-yahir-4.webp',
+    image: '/img/Memes/Meme-7-bechito.webp',
     class: 'sm:row-start-2 sm:row-end-4 sm:col-start-2 sm:col-end-3 sm:h-[300px] col-start-2 col-end-3 row-start-2 row-end-5 h-[300px]'
   },
   {
     id: 'Meme-8-mirada-folladora',
-    image: '/img/Memes/image-meme-elvin-5.webp',
+    image: '/img/Memes/Meme-8-mirada-folladora.webp',
     class: 'sm:col-start-4 sm:col-end-5 sm:row-start-2 sm:row-end-4 sm:h-[300px] col-start-2 col-end-3 row-start-6 row-end-8 h-[300px]'
   }
 ]
