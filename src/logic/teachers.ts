4746db725c252cc17a9c7fe2a3a68e12748c419f
MartL7 <latiapelitos123@gmail.com>
2024-04-30 00:22:07 -0600

    🤍 Added teacher category

---
+++ src/logic/teachers.ts
@@ -0,0 +1,24 @@
+import type { Teacher } from "@/types/teachers"
+
+export const teachers: Teacher[] = [
+  {
+    id: 'Dayanne',
+    name: 'Dayanne',
+    imagePresentation: '/img/Profes/Dayanne/Dayanne-image-presentation.webp'
+  },
+  {
+    id: 'Lizzy',
+    name: 'Lizzy',
+    imagePresentation: '/img/Profes/Lizzy/Lizzy-image-presentation.webp'
+  },
+  {
+    id: 'Dayanne',
+    name: 'Dayanne',
+    imagePresentation: '/img/Profes/Dayanne/Dayanne-image-presentation.webp'
+  },
+  {
+    id: 'Lizzy',
+    name: 'Lizzy',
+    imagePresentation: '/img/Profes/Lizzy/Lizzy-image-presentation.webp'
+  }
+]
---
+++ src/types/teachers.ts
@@ -0,0 +1,5 @@
+export interface Teacher {
+  id: string
+  name: string
+  imagePresentation: string
+}
