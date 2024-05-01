c24806aa87c9aa1baf68182314f91241beab0a51
MartL7 <latiapelitos123@gmail.com>
2024-05-01 01:43:56 -0600

    test

--- db/seed.ts
+++ db/seed.ts
@@ -4,6 +4,7 @@ import { Categories as CategoryId } from '@/logic/categories'
 
 // https://astro.build/db/seed
 export default async function seed() {
-	const categories = CategoryId.map(({ id, name }) => ({ id, name }))
+	const categories = CategoryId.map((id) => ({ id }))
+
 	await db.insert(Categories).values(categories)
 }
--- src/logic/categories.ts
+++ src/logic/categories.ts
@@ -1,10 +1,4 @@
 export const Categories = [
-  {
-    id: 'category-memes',
-    name: 'category-memes'
-  },
-  {
-    id: 'category-teachers',
-    name: 'category-teachers'
-  }
+  'category-memes',
+  'category-teachers'
 ]
