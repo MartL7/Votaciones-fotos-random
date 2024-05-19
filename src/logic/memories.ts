a281b72d515f02233a6129d4a737aa79f75d9f75
MartL7 <latiapelitos123@gmail.com>
2024-05-19 14:49:27 -0600

    😸 Added section for memories

---
+++ src/logic/memories.ts
@@ -0,0 +1,33 @@
+interface Memories {
+  id: string
+  image: string
+  className: string
+}
+
+export const memories: Memories[] = [
+  {
+    id: 'memorie-papus-1',
+    image: '/img/Memories/Memorie-papus-1.avif',
+    className: 'md:col-start-1 md:col-end-3 md:h-[300px] h-[250px]'
+  },
+  {
+    id: 'memorie-papus-2',
+    image: '/img/Memories/Memorie-papus-2.avif',
+    className: 'md:col-start-3 md:col-end-4 md:h-[300px] h-[250px]'
+  },
+  {
+    id: 'memorie-papus-3',
+    image: '/img/Memories/Memorie-papus-3.avif',
+    className: 'md:col-start-4 md:col-end-5 md:h-[300px] h-[250px]'
+  },
+  {
+    id: 'memorie-papus-4',
+    image: '/img/Memories/Memorie-papus-4.avif',
+    className: 'md:col-start-1 md:col-end-3 md:h-[300px]'
+  },
+  {
+    id: 'memorie-papus-5',
+    image: '/img/Memories/Memorie-papus-5.avif',
+    className: 'md:col-start-3 md:col-end-5 md:h-[300px]' 
+  }
+]
