8f11789a731209f7c7431118fb5dc61490442e3f
MartL7 <latiapelitos123@gmail.com>
2024-05-24 00:17:25 -0600

    😸 Change design and added logic for ship

---
+++ src/const/showShip.ts
@@ -0,0 +1,19 @@
+// -> Agregar cuando lo optimice más
+
+/* if (id.toLowerCase() === 'elvin') {
+  $imageLink.forEach(($image) => {
+    const { id } = $image.dataset ?? null
+    if (id.toLocaleLowerCase() === 'ale' || id.toLowerCase() === 'janet') {
+      const $characterSmallImage = $$(`#character-${id}`) as globalThis.NodeListOf<HTMLImageElement>
+      $characterSmallImage.forEach((image) => image.classList.add('ship'))
+    }
+  })
+} else {
+  $imageLink.forEach(($image) => {
+    const { id } = $image.dataset ?? null
+    if (id.toLocaleLowerCase() === 'ale' || id.toLowerCase() === 'janet') {
+      const $characterSmallImage = $$(`#character-${id}`) as globalThis.NodeListOf<HTMLImageElement>
+      $characterSmallImage.forEach((image) => image.classList.remove('ship'))
+    }
+  })
+} */
