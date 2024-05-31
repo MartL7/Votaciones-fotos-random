252e1d866e21e69fcfb0c8b70fd7fe6d84af7870
MartL7 <latiapelitos123@gmail.com>
2024-05-31 17:03:14 -0600

    😺 Added section of results

--- src/logic/results.ts
+++ src/logic/results.ts
@@ -22,9 +22,34 @@ export const results: Results[] = [
   },
 
   {
-    id: 'result-ever',
-    name: 'Ever',
-    category: 'Clip-del-ciclo',
-    videoThumbnail: ''
+    id: 'result-teachers',
+    name: 'Dianne',
+    imagePresentation: '/img/Profes/Dayanne/Dayanne-image-presentation.webp',
+    category: 'Category-teachers',
+    images: [
+      { image: '/img/Profes/Dayanne/Dianne-image-1.webp', 
+        class: 'md:col-start-1 md:col-end-2 md:row-start-1 md:row-end-2 md:h-[350px] col-start-2 col-end-3 row-start-1 row-end-2 h-[200px]' 
+      },
+
+      { image: '/img/Profes/Dayanne/Dianne-image-2.webp',
+        class: 'md:col-start-2 md:col-end-3 md:row-start-1 md:row-end-2 md:h-[350px] col-start-1 col-end-2 row-start-3 row-end-4 h-[200px]' 
+      },
+
+      { image: '/img/Profes/Dayanne/Dianne-image-3.webp',
+        class: 'md:col-start-3 md:col-end-4 md:row-start-1 md:row-end-2 md:h-[350px] col-start-1 col-end-2 row-start-1 row-end-3 h-[350px]'  
+      },
+
+      {
+        image: '/img/Profes/Dayanne/Dianne-image-4.webp',
+        class: 'md:col-start-4 md:col-end-5 md:h-[350px] md:row-start-1 md:row-end-2 col-start-2 col-end-3 row-start-2 row-end-4 h-[350px]'
+      }
+    ]
+  },
+
+  {
+    id: 'result-memes',
+    name: 'Meme de Martín',
+    imagePresentation: '/img/Memes/Meme-2-xd.webp',
+    category: 'Category-memes'
   }
 ]
