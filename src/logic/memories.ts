39bb4beb03532e18895649c8be35a9024898a043
c0d3hdz <marquezhernandez.damian@gmail.com>
2024-06-01 00:52:39 -0600

    'visual-bug-fix-and-new-group-members-section'

--- src/logic/memories.ts
+++ src/logic/memories.ts
@@ -1,33 +1,71 @@
 interface Memories {
-  id: string
-  image: string
-  className: string
+    id: string
+    image: string
+    className: string
+}
+interface Member {
+    id: string
+    class_member: string
 }
 
 export const memories: Memories[] = [
-  {
-    id: 'memorie-papus-1',
-    image: '/img/Memories/Memorie-papus-1.avif',
-    className: 'md:col-start-1 md:col-end-3 md:h-[300px] h-[250px]'
-  },
-  {
-    id: 'memorie-papus-2',
-    image: '/img/Memories/Memorie-papus-2.avif',
-    className: 'md:col-start-3 md:col-end-4 md:h-[300px] h-[250px]'
-  },
-  {
-    id: 'memorie-papus-3',
-    image: '/img/Memories/Memorie-papus-3.avif',
-    className: 'md:col-start-4 md:col-end-5 md:h-[300px] h-[250px]'
-  },
-  {
-    id: 'memorie-papus-4',
-    image: '/img/Memories/Memorie-papus-4.avif',
-    className: 'md:col-start-1 md:col-end-3 md:h-[300px]'
-  },
-  {
-    id: 'memorie-papus-5',
-    image: '/img/Memories/Memorie-papus-5.avif',
-    className: 'md:col-start-3 md:col-end-5 md:h-[300px]' 
-  }
+    {
+        id: 'memorie-papus-1',
+        image: '/img/Memories/Memorie-papus-1.avif',
+        className: 'md:col-start-1 md:col-end-3 md:h-[300px] h-[250px]',
+    },
+    {
+        id: 'memorie-papus-2',
+        image: '/img/Memories/Memorie-papus-2.avif',
+        className: 'md:col-start-3 md:col-end-4 md:h-[300px] h-[250px]',
+    },
+    {
+        id: 'memorie-papus-3',
+        image: '/img/Memories/Memorie-papus-3.avif',
+        className: 'md:col-start-4 md:col-end-5 md:h-[300px] h-[250px]',
+    },
+    {
+        id: 'memorie-papus-4',
+        image: '/img/Memories/Memorie-papus-4.avif',
+        className: 'md:col-start-1 md:col-end-3 md:h-[300px]',
+    },
+    {
+        id: 'memorie-papus-5',
+        image: '/img/Memories/Memorie-papus-5.avif',
+        className: 'md:col-start-3 md:col-end-5 md:h-[300px]',
+    },
+]
+export const member: Member[] = [
+    {
+        id: 'Damian',
+        class_member: 'papus',
+    },
+    {
+        id: 'Geovas',
+        class_member: 'papus',
+    },
+    {
+        id: 'Elvin',
+        class_member: 'papus',
+    },
+    {
+        id: 'Antonio',
+        class_member: 'papus',
+    },
+    {
+        id: 'Yahir',
+        class_member: 'papus',
+    },
+    {
+        id: 'Karen',
+        class_member: 'papus',
+    },
+    {
+        id: 'Ulises',
+        class_member: 'papus',
+    },
+    {
+        id: 'CruCru',
+        class_member: 'papus',
+    },
 ]
