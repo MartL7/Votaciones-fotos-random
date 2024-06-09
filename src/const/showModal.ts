5f14a284c57105795072f8b46620f974c57bad71
MartL7 <latiapelitos123@gmail.com>
2024-06-09 00:36:51 -0600

    ♥️ Added animation for open and close Modal

--- src/const/showModal.ts
+++ src/const/showModal.ts
@@ -18,15 +18,30 @@ export const showModal = () => {
     image.addEventListener('click', () => {
       const name = url.split('/').at(-2)
 
-      $imageClip.classList.add('animate-fade-in')
+      $imageClip.classList.remove('animate-zoom-out')
+      $imageClip.classList.add('animate-zoom-in')
       $imageClip.alt = `Imagen de la galeria de ${name}`
       $imageClip.src = url
       $imageDialog.showModal()
 
-      $closeButton.onclick = () => $imageDialog.close()
+      $closeButton.onclick = () => {
+        $imageClip.classList.remove('animate-zoom-in')
+        $imageClip.classList.add('animate-zoom-out')
+
+        $imageClip.addEventListener('animationend', () => {
+          $imageDialog.close()
+        }, { once: true })
+      }
 
       $imageDialog.onclick = (event) => {
-        if (event.target === $imageDialog) $imageDialog.close()
+        if (event.target === $imageDialog) {
+          $imageClip.classList.remove('animate-zoom-in')
+          $imageClip.classList.add('animate-zoom-out')
+
+          $imageClip.addEventListener('animationend', () => {
+            $imageDialog.close()
+          }, { once: true })
+        }
       }
     })
   })
