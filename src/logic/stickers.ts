3a5482f5d32bb6f909ab673b1069a058de5c57f3
MartL7 <latiapelitos123@gmail.com>
2024-06-20 16:23:22 -0600

    😎 Added new stcikers for comments

--- src/logic/stickers.ts
+++ src/logic/stickers.ts
@@ -45,5 +45,110 @@ export const stickers: Sticker[] = [
     id: 'Sticker-7',
     code: '#ss7',
     imageUrl: '/img/Stickers/ss7.webp'
+  },
+
+  {
+    id: 'Sticker-8',
+    code: '#ss8',
+    imageUrl: '/img/Stickers/ss8.gif'
+  },
+
+  {
+    id: 'Sticker-9',
+    code: '#ss9',
+    imageUrl: '/img/Stickers/ss9.webp'
+  },
+
+  {
+    id: 'Sticker-10',
+    code: '#ss10',
+    imageUrl: '/img/Stickers/ss10.webp'
+  },
+/*   {
+    id: 'Sticker-11',
+    code: '#ss11',
+    imageUrl: '/img/Stickers/ss11.webp'
+  }, */
+  {
+    id: 'Sticker-12',
+    code: '#ss12',
+    imageUrl: '/img/Stickers/ss12.webp'
+  },
+  {
+    id: 'Sticker-13',
+    code: '#ss13',
+    imageUrl: '/img/Stickers/ss13.webp'
+  },
+  {
+    id: 'Sticker-14',
+    code: '#ss14',
+    imageUrl: '/img/Stickers/ss14.webp'
+  },
+  {
+    id: 'Sticker-15',
+    code: '#ss15',
+    imageUrl: '/img/Stickers/ss15.webp'
+  },
+  {
+    id: 'Sticker-16',
+    code: '#ss16',
+    imageUrl: '/img/Stickers/ss16.webp'
+  },
+  {
+    id: 'Sticker-17',
+    code: '#ss17',
+    imageUrl: '/img/Stickers/ss17.webp'
+  },
+  {
+    id: 'Sticker-18',
+    code: '#ss18',
+    imageUrl: '/img/Stickers/ss18.webp'
+  },
+  {
+    id: 'Sticker-19',
+    code: '#ss19',
+    imageUrl: '/img/Stickers/ss19.webp'
+  },
+  {
+    id: 'Sticker-20',
+    code: '#ss20',
+    imageUrl: '/img/Stickers/ss20.webp'
+  },
+  {
+    id: 'Sticker-21',
+    code: '#ss21',
+    imageUrl: '/img/Stickers/ss21.webp'
+  },
+  {
+    id: 'Sticker-22',
+    code: '#ss22',
+    imageUrl: '/img/Stickers/ss22.webp'
+  },
+  {
+    id: 'Sticker-23',
+    code: '#ss23',
+    imageUrl: '/img/Stickers/ss23.webp'
+  },
+  {
+    id: 'Sticker-24',
+    code: '#ss24',
+    imageUrl: '/img/Stickers/ss24.webp'
+  },
+  {
+    id: 'Sticker-25',
+    code: '#ss25',
+    imageUrl: '/img/Stickers/ss25.webp'
+  },
+  {
+    id: 'Sticker-26',
+    code: '#ss26',
+    imageUrl: '/img/Stickers/ss26.webp'
+  },
+  {
+    id: 'Sticker-27',
+    code: '#ss27',
+    imageUrl: '/img/Stickers/ss27.gif'
   }
+  
+  
 ]
