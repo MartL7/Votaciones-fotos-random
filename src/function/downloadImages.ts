ff209cc1e603e2ac818d9665dab3113f05fc0244
MartL7 <latiapelitos123@gmail.com>
2024-08-11 00:44:19 -0600

    😄 Added function for download images

---
+++ src/function/downloadImages.ts
@@ -0,0 +1,28 @@
+import { toastAlert } from '@/function/alerts'
+
+interface Props {
+  $buttonDownload: HTMLButtonElement
+  $containerImages: NodeListOf<HTMLElement>
+}
+
+export function downloadImages ({ $buttonDownload, $containerImages }: Props): void {
+  const $svgIcon = $buttonDownload.querySelector('svg') as SVGSVGElement
+
+  $buttonDownload.disabled = true
+  $svgIcon.classList.add('opacity-30')
+
+  toastAlert(
+    { title: 'Descargando', type: 'info', icon: true, dismissible: false, location: 'bottom-center' }
+  )
+
+  $containerImages.forEach(($image) => {
+    const $imageToDownload = $image.querySelector('img') as HTMLImageElement
+    const { src, alt } = $imageToDownload
+
+    const a = document.createElement('a')
+    a.href = src
+    a.download = alt
+
+    a.click()
+  })
+}
